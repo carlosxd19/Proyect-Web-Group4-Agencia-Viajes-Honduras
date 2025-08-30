384dd0b2c8db8d72ca5194411e0fa565c89927c3
carlos <161674271+carlosxd19@users.noreply.github.com>
2025-08-30 15:18:15 -0600

    feat: Agregando la licencia MIT
    Se agrega para poder ser reutilizable o modificar codigos en el proyecto

--- AgenciaViajesBackend_NET8_FULL/AgenciaViajes.API/obj/Debug/net8.0/AgenciaViajes.API.AssemblyInfo.cs
+++ AgenciaViajesBackend_NET8_FULL/AgenciaViajes.API/obj/Debug/net8.0/AgenciaViajes.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgenciaViajes.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16eb39ab63eed455a2cce21fce7883101df07b3e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7530a61689d0a11b83dc72396ad8c04d59110e1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgenciaViajes.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgenciaViajes.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
