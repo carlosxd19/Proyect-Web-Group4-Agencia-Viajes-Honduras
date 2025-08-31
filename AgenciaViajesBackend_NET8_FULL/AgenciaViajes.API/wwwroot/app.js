155dcd4b519e73f5ecb746838f9f57e177ad7262
carlos <161674271+carlosxd19@users.noreply.github.com>
2025-08-31 10:52:33 -0600

    feat: Agregando metodos al login.
    Agregando metodos en los aspectos locales del https para cuando inicie sesion con datos creados en el archivo js al ingresar en el boton de iniciar sesion entre directo al interfaz swagger.

--- AgenciaViajesBackend_NET8_FULL/AgenciaViajes.API/wwwroot/app.js
+++ AgenciaViajesBackend_NET8_FULL/AgenciaViajes.API/wwwroot/app.js
@@ -6,9 +6,16 @@ document.getElementById("loginForm").addEventListener("submit", function (event)
   const message = document.getElementById("message");
 
   // Usuario y contraseña de ejemplo
-  if (user === "admin" && pass === "1234") {
+  if (user === "cd_19" && pass === "Carlos2004") {
     message.style.color = "lime";
     message.textContent = "✅ Inicio de sesión exitoso";
+
+    // Espera un momento para mostrar el mensaje y luego redirige a Swagger
+    setTimeout(() => {
+      window.location.href = "https://localhost:7053/swagger";
+      // Cambia el puerto si tu API usa otro
+    }, 800);
+
   } else {
     message.style.color = "red";
     message.textContent = "❌ Usuario o contraseña incorrectos";
