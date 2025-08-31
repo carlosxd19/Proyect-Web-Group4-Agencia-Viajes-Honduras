4fcb9f49f26cc3c8d405fab4f948a2c0c423fa4a
carlos <161674271+carlosxd19@users.noreply.github.com>
2025-08-31 11:01:08 -0600

    feat:cambio del nombre del fondo.

--- AgenciaViajesBackend_NET8_FULL/AgenciaViajes.API/obj/Debug/net8.0/AgenciaViajes.API.AssemblyInfo.cs
+++ AgenciaViajesBackend_NET8_FULL/AgenciaViajes.API/obj/Debug/net8.0/AgenciaViajes.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgenciaViajes.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+155dcd4b519e73f5ecb746838f9f57e177ad7262")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1eb1cccded634922c8c53b63821836907cb73a13")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgenciaViajes.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgenciaViajes.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
